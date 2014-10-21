3e228d853d9b5b6f46abb4337497f724bddbfed9
David Morrison <me@davemor.com>
2014-10-21 09:29:25 +0100

    Update the package.json file. Add multer to app.js includes. Improve error and index views.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var multer  = require('multer');
 var routes = require('./routes/index');
 
 var app = express();
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ dest: './uploads/'}));
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,79 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var pdf_extract = require('pdf-extract');
 
-/* GET home page. */
+/* GET form for uploading file by human */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OCR Service' });
 });
+
+var error = function(res, message, code) {
+  res.render('error', {
+    message: message,
+    code: code
+  });
+}
+
+var errorOCR = function(res, err) {
+  console.log("Processing aborted due to error.")
+  console.log(err);
+  error(res, "OCR process failed with error: " + err, 500);
+}
+
+/* POST a pdf to process */
+router.post('/', function(req, res) {
+  var pdfFile = req.files.pdfFile;
+
+  // do some error checking
+  if(pdfFile == null) {
+    error(res,"No file attached", 400);
+  } else if (req.files.pdfFile.mimetype != 'application/pdf') {
+    error(res, "Uploaded file is not pdf", 400);
+  } else {
+      // get the file logation
+      var appDir = path.dirname(require.main.filename);
+      var absolutePathToFile = path.resolve("./uploads/" + pdfFile.name);
+      
+      // logging
+      console.log("PDF uploaded to " + absolutePathToFile);
+      console.log("Starting to process ...");
+
+      // run the ocr process
+      var processor = pdf_extract(absolutePathToFile, {type:'ocr'}, function(err) {
+        if(err) {
+          errorOCR(res, err);
+       } else {
+          console.log("Processing exited without error.")
+        }
+      });
+      processor.on('complete', function(data) {
+        console.log("OCR complete.");
+        console.log(data);
+        success(req, res, data);
+      });
+      processor.on('error', function(err) {
+        return errorOCR(res, err);
+      });
+  }
+});
+
+var success = function(req, res, data) {
+  // respond based on content negotiation
+  res.format({
+    html: function() {
+      res.render('text', {
+        title: req.files.pdfFile.originalname,
+        pages: data.text_pages
+      })
+    },
+    json: function() {
+      res.json( {
+        hash: data.hash,
+        text_pages: data.text_pages
+      });
+    }
+  })
+}
 
 module.exports = router;
